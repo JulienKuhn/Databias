92e88a0babc265c680069a0ac26dfdafd55174be
JulienKuhn <58598280+JulienKuhn@users.noreply.github.com>
2025-11-21 18:32:50 +0100

    Languagedetector

--- databias/src/i18n.ts
+++ databias/src/i18n.ts
@@ -1,24 +1,31 @@
+// i18n.ts
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import Backend from 'i18next-http-backend';
+import LanguageDetector from 'i18next-browser-languagedetector';
 
 i18n
-    .use(Backend) 
+    .use(LanguageDetector)
+    .use(Backend)
     .use(initReactI18next)
     .init({
         backend: {
             loadPath: '/locales/{{lng}}/{{ns}}.json',
         },
-        lng: "en",
-        fallbackLng: "fr",
+        fallbackLng: "en", 
         ns: "translation",
         defaultNS: "translation",
+        detection: {
+            order: ['localStorage', 'navigator'],
+            lookupLocalStorage: 'i18nextLng',
+            caches: ['localStorage'],
+        },
         interpolation: {
             escapeValue: false,
         },
         react: {
             useSuspense: false
-        }
+        },
     });
 
 export default i18n;
